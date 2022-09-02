a40bff33701309511aa7192fdeba5a9213997f92
cloer <chltjrrb97@gmail.com>
2022-09-01 22:50:00 -0400

    UPDATE: BrowserRouter basename

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Bet } from './pages';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/playground'>
       <Routes>
         <Route path='/' element={<CenterLayout />}>
           <Route index element={<Bet />} />
